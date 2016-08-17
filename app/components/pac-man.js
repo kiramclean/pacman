1dfbb43c7124eea2f008725d3d25d8f29a7f7904
Kira McLean <kiraemclean@gmail.com>
2016-08-16 20:59:31 -0400

    make pacman loop around screen

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -5,49 +5,82 @@ export default Ember.Component.extend(KeyboardShortcuts, {
   didInsertElement: function() {
     this.drawCircle();
   },
-  movePacMan: function(direction, amount) {
-    this.incrementProperty(direction, amount);
-    this.clearScreen();
-    this.drawCircle();
-  },
+
   ctx: Ember.computed(function() {
     let canvas = document.getElementById('canvas');
     let ctx = canvas.getContext('2d');
     return ctx;
   }),
-  x: 50,
-  y: 100,
+
+  x: 1,
+  y: 2,
   squareSize: 40,
+  screenWidth: 20,
+  screenHeight: 15,
+
   clearScreen: function() {
     let ctx = this.get('ctx');
-    let screenWidth = 800;
-    let screenHeight = 600;
-    ctx.clearRect(0, 0, screenWidth, screenHeight);
+    let screenPixelWidth = this.get('screenWidth') * this.get('squareSize');
+    let screenPixelHeight = this.get('screenHeight') * this.get('squareSize');
+    ctx.clearRect(0, 0, screenPixelWidth , screenPixelHeight);
   },
+
   drawCircle: function() {
     let ctx = this.get('ctx');
     let x = this.get('x');
     let y = this.get('y');
-    let radius = this.get('squareSize')/2;
+    let squareSize = this.get('squareSize');
+
+    let pixelX = (x+1/2) * squareSize;
+    let pixelY = (y+1/2) * squareSize;
 
     ctx.fillStyle = '#000';
     ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+    ctx.arc(pixelX, pixelY, squareSize/2, 0, Math.PI * 2, false);
     ctx.closePath();
     ctx.fill();
   },
+
   keyboardShortcuts: {
     up: function() {
-      this.movePacMan('y', -1 * this.get('squareSize'));
+      this.movePacMan('y', -1);
     },
     down: function() {
-      this.movePacMan('y', this.get('squareSize'));
+      this.movePacMan('y', 1);
     },
     right: function() {
-      this.movePacMan('x', this.get('squareSize'));
+      this.movePacMan('x', 1);
     },
     left: function() {
-      this.movePacMan('x', -1 * this.get('squareSize'));
+      this.movePacMan('x', -1);
     },
+  },
+
+  movePacMan: function(direction, amount) {
+    this.incrementProperty(direction, amount);
+
+    if (this.collidedWithLeftRightBorder()) {
+      this.decrementProperty(direction, amount*this.get('screenWidth'))
+    }
+    if (this.collidedWithUpDownBorder()) {
+      this.decrementProperty(direction, amount*this.get('screenHeight'))
+    }
+
+    this.clearScreen();
+    this.drawCircle();
+  },
+
+  collidedWithLeftRightBorder: function() {
+    let x = this.get('x');
+    let screenWidth = this.get('screenWidth');
+    let pacOutOfBounds = x < 0 || x >= screenWidth;
+    return pacOutOfBounds
+  },
+
+  collidedWithUpDownBorder: function() {
+    let y = this.get('y');
+    let screenHeight = this.get('screenHeight');
+    let pacOutOfBounds = y < 0 || y >= screenHeight;
+    return pacOutOfBounds
   }
 });
